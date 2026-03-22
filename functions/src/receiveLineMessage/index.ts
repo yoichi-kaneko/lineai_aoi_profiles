8c4da4d98323d44e8b98809eb92234b505e81696
Yoichi Kaneko <kaneko@webstudio-wanderlust.voyage>
2026-03-22 16:24:40 +0900

    Cloud Functionの実装途中

--- functions/src/receiveLineMessage/index.ts
+++ functions/src/receiveLineMessage/index.ts
@@ -1,14 +1,16 @@
 import { Firestore, Timestamp } from "@google-cloud/firestore";
 import * as functions from "@google-cloud/functions-framework";
+import { webhook } from "@line/bot-sdk";
 
 const firestore = new Firestore();
 
 functions.http("receiveLineMessage", async (req, res) => {
-  const body = req.body as { events?: LineEvent[] };
+  const body = req.body as webhook.CallbackRequest;
   const events = body.events ?? [];
 
   for (const event of events) {
-    if (event.type === "message" && event.message?.type === "text") {
+    if (event.type === "message" && event.message.type === "text") {
+      const message = event.message as webhook.TextMessageContent;
       const receivedAt = new Date(event.timestamp);
       const year = receivedAt.getFullYear();
       const month = receivedAt.getMonth();
@@ -17,26 +19,39 @@ functions.http("receiveLineMessage", async (req, res) => {
 
       await firestore.collection("notes").add({
         date: Timestamp.fromDate(dateValue),
-        descript: event.message.text,
+        description: message.text,
         type: "line_text",
         isRead: false,
         createdAt: Timestamp.fromDate(new Date()),
       });
+    } else if (
+      event.type === "message" &&
+      event.message.type === "image" &&
+      (event.message as webhook.ImageMessageContent).contentProvider.type ===
+        "line"
+    ) {
+      const message = event.message as webhook.ImageMessageContent;
+      const receivedAt = new Date(event.timestamp);
+      const year = receivedAt.getFullYear();
+      const month = receivedAt.getMonth();
+      const day = receivedAt.getDate();
+      const dateValue = new Date(year, month, day);
+
+      await firestore.collection("notes").add({
+        date: Timestamp.fromDate(dateValue),
+        description: JSON.stringify({ id: message.id }),
+        type: "line_image",
+        isRead: false,
+        createdAt: Timestamp.fromDate(new Date()),
+      });
     } else {
+      const messageType =
+        event.type === "message" ? event.message.type : "N/A";
       throw new Error(
-        `Unsupported event: type=${event.type}, message.type=${event.message?.type ?? "N/A"}`
+        `Unsupported event: type=${event.type}, message.type=${messageType}`
       );
     }
   }
 
   res.status(200).send("OK");
 });
-
-interface LineEvent {
-  type: string;
-  timestamp: number;
-  message?: {
-    type: string;
-    text?: string;
-  };
-}
